36bfdf6bddbdfa8e1e51ddb0cd9468c0b575485a
dlavery <dlavery62@gmail.com>
2019-04-16 13:08:58 +0100

    use predetermined IV

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const aes256gcm = (key) => {
   const ALGO = 'aes-256-gcm';
 
   // encrypt returns base64-encoded ciphertext
-  const encrypt = (str) => {
+  const encrypt = (str, iv) => {
     // Hint: the `iv` should be unique (but not necessarily random).
     // `randomBytes` here are (relatively) slow but convenient for
     // demonstration.
-    const iv = new Buffer.alloc(16, crypto.randomBytes(16), 'utf8');
+    //const iv = new Buffer.alloc(16, crypto.randomBytes(16), 'utf8');
     const cipher = crypto.createCipheriv(ALGO, key, iv);
 
     // Hint: Larger inputs (it's GCM, after all!) should use the stream API
     let enc = cipher.update(str, 'utf8', 'base64');
     enc += cipher.final('base64');
-    return [enc, iv, cipher.getAuthTag()];
+    return [enc, cipher.getAuthTag()];
   };
 
   // decrypt decodes base64-encoded ciphertext into a utf8-encoded string
@@ -35,11 +35,12 @@ const aes256gcm = (key) => {
 };
 
 const KEY = new Buffer.alloc(32, crypto.randomBytes(32), 'utf8');
+const IV = new Buffer.alloc(16, 'test init vector', 'utf8')
+const aesCipher = aes256gcm(KEY, IV);
 
-const aesCipher = aes256gcm(KEY);
-
-const [encrypted, iv, authTag] = aesCipher.encrypt('hello, world');
+const [encrypted, authTag] = aesCipher.encrypt('goodbye, world', IV);
 console.log(encrypted); // 'hello, world' encrypted
+//console.log(authTag);
 
-const decrypted = aesCipher.decrypt(encrypted, iv, authTag);
+const decrypted = aesCipher.decrypt(encrypted, IV, authTag);
 console.log(decrypted); // 'hello, world'
