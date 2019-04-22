5e8cf4fb9b17ea57c83dd76c81c72c0ce73dbffc
David Lavery <dlavery62@gmail.com>
2019-04-22 16:59:27 +0100

    implement encryption as an RESTful service

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 const buffer = require('buffer');
 const crypto = require('crypto');
 const AWS = require('aws-sdk')
+const express = require('express')
 
-// Demo implementation of using `aes-256-gcm` with node.js's `crypto` lib.
-const aes256gcm = (key, iv) => {
+const app = express();
+const port = 8080;
+
+// Implementation of `aes-256-gcm` with node.js's `crypto` lib.
+const aes256gcm = (key, iv, cipherKey) => {
   const ALGO = 'aes-256-gcm';
 
   // encrypt returns base64-encoded ciphertext
-  const encrypt = (str) => {
+  const encrypt = (buf) => {
     const cipher = crypto.createCipheriv(ALGO, key, iv);
-    let enc = cipher.update(str, 'utf8', 'base64');
+    let enc = cipher.update(buf, null, 'base64');
     enc += cipher.final('base64');
-    return cipher.getAuthTag().toString('base64')+enc;
+    return cipherKey.toString('base64') + cipher.getAuthTag().toString('base64') + enc;
   };
 
   // decrypt decodes base64-encoded ciphertext into a utf8-encoded string
   const decrypt = (enc) => {
-    authTag = Buffer.from(enc.substring(0,23), 'base64');
-    const decipher = crypto.createDecipheriv(ALGO, key, iv);
-    decipher.setAuthTag(authTag);
-    let str = decipher.update(enc.substring(24), 'base64', 'utf8');
-    str += decipher.final('utf8');
-    return str;
+    decryptKey = Buffer.from(enc.substring(0, 247), 'base64');
+    authTag = Buffer.from(enc.substring(248, 271), 'base64');
+    kms.decrypt({CiphertextBlob: decryptKey}, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else {
+        const decipher = crypto.createDecipheriv(ALGO, data.Plaintext, iv);
+        decipher.setAuthTag(authTag);
+        let buf = decipher.update(enc.substring(272), 'base64');
+        buf += decipher.final();
+        return buf;
+      }
+      //console.log(data);           // successful response
+       /*
+       data = {
+        KeyId: "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab", // The Amazon Resource Name (ARN) of the CMK that was used to decrypt the data.
+        Plaintext: <Binary String>// The decrypted (plaintext) data.
+       }
+       */
+     });
   };
 
   return {
@@ -31,26 +48,49 @@ const aes256gcm = (key, iv) => {
 };
 
 // Get KMS Key
-const kms = new AWS.KMS({region: 'eu-west-2'});
+const kms = new AWS.KMS({
+  region: 'eu-west-2'
+});
 const params = {
-    KeyId: "arn:aws:kms:eu-west-2:713186966590:key/0a086f1b-67e1-4765-afc6-a90c1e7018c2",
-    KeySpec: "AES_256"
+  KeyId: "arn:aws:kms:eu-west-2:713186966590:key/0a086f1b-67e1-4765-afc6-a90c1e7018c2",
+  KeySpec: "AES_256"
 };
-//var KEY;
+
 kms.generateDataKey(params, function(err, data) {
   if (err) {
     console.log(err, err.stack); // an error occurred
-  }
-  else {
+  } else {
     const dataKey = data.Plaintext.toString('utf8');
     const KEY = new Buffer.alloc(32, dataKey, 'utf8');
     const IV = new Buffer.alloc(16, '0123456789abcdef', 'utf8')
-    const aesCipher = aes256gcm(KEY, IV);
+    const aesCipher = aes256gcm(KEY, IV, data.CiphertextBlob);
 
-    const encrypted = aesCipher.encrypt('goodbye, cruel world');
-    console.log(encrypted); // 'hello, world' encrypted
+    //const encrypted = aesCipher.encrypt(Buffer.from('goodbye, cruel world', 'utf8'));
+    //console.log(encrypted); // 'hello, world' encrypted
 
-    const decrypted = aesCipher.decrypt(encrypted);
-    console.log(decrypted); // 'hello, world'
-   }
- });
+    //const decrypted = aesCipher.decrypt(encrypted);
+    //console.log(decrypted.toString('utf8')); // 'hello, world'
+
+    app.use('/encryptdata', express.json());
+    app.post('/encryptdata', function (req, res) {
+      data = {};
+      for (var attr in req.body)
+        if (req.body.hasOwnProperty(attr)) {
+          data[attr] = aesCipher.encrypt(Buffer.from(req.body[attr], 'utf8'));
+        }
+      res.send(JSON.stringify(data));
+    });
+
+    app.use('/decryptdata', express.json());
+    app.post('/decryptdata', function (req, res) {
+      data = {};
+      for (var attr in req.body)
+        if (req.body.hasOwnProperty(attr)) {
+          data[attr] = aesCipher.decrypt(req.body[attr]).toString('utf8');
+        }
+      res.send(JSON.stringify(data));
+    });
+
+    app.listen(port, () => console.log(`Encryption Service running on port ${port}.`));
+  }
+});
