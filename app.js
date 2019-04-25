8172239904ac3d93dffddbf01c8caf874832ea24
dlavery <dlavery62@gmail.com>
2019-04-25 14:49:50 +0100

    add secrets manager

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const Q = require('q');
 
 const app = express();
 const port = 8080;
+const region = "eu-west-2";
 
 // Implementation of `aes-256-gcm` with node.js's `crypto` lib.
 const aes256gcm = (key, iv) => {
@@ -37,7 +38,7 @@ const aes256gcm = (key, iv) => {
 
 // Get KMS Key
 const kms = new AWS.KMS({
-  region: 'eu-west-2'
+  region: region
 });
 
 const params = {
@@ -47,9 +48,64 @@ const params = {
 
 const initVector = '0123456789abcdef';
 
+/*
+var AWS = require('aws-sdk'),
+    region = "eu-west-2",
+    secretName = "encrypt-data-service",
+    secret,
+    decodedBinarySecret;
+
+// Create a Secrets Manager client
+var client = new AWS.SecretsManager({
+    region: region
+});
+
+// In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+// See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+// We rethrow the exception by default.
+
+client.getSecretValue({SecretId: secretName}, function(err, data) {
+    if (err) {
+        if (err.code === 'DecryptionFailureException')
+            // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'InternalServiceErrorException')
+            // An error occurred on the server side.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'InvalidParameterException')
+            // You provided an invalid value for a parameter.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'InvalidRequestException')
+            // You provided a parameter value that is not valid for the current state of the resource.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'ResourceNotFoundException')
+            // We can't find the resource that you asked for.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+    }
+    else {
+        // Decrypts secret using the associated KMS CMK.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if ('SecretString' in data) {
+            secret = data.SecretString;
+        } else {
+            let buff = new Buffer(data.SecretBinary, 'base64');
+            decodedBinarySecret = buff.toString('ascii');
+        }
+    }
+
+    // Your code goes here.
+});
+*/
+
 // Process JSON tree
 const jsonProcess = (tree, fn) => {
   for (var attr in tree) {
+    if (!tree.hasOwnProperty(attr)) continue;
     if (typeof tree[attr] !== 'object') {
       fn(tree, attr);
       continue;
@@ -111,8 +167,6 @@ kms.generateDataKey(params, function(err, data) {
               let PLAINKEY = new Buffer.alloc(32, plainKey, 'utf8');
               let plaintext = aesCipher.decrypt(encryptedData, PLAINKEY);
               node[attr] = plaintext;
-              console.log(JSON.stringify(result));
-              console.log('----');
               deferred.resolve(true);
             }
             catch (err) {
--- test.js
+++ test.js
@@ -5,6 +5,54 @@ const express = require('express')
 
 const app = express();
 const port = 8080;
+const region = "eu-west-2";
+
+// Create a Secrets Manager client
+var client = new AWS.SecretsManager({
+    region: region
+});
+
+// In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+// See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+// We rethrow the exception by default.
+
+client.getSecretValue({SecretId: "encrypt-data-service"}, function(err, data) {
+    if (err) {
+        console.log(err, err.stack);
+        if (err.code === 'DecryptionFailureException')
+            // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'InternalServiceErrorException')
+            // An error occurred on the server side.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'InvalidParameterException')
+            // You provided an invalid value for a parameter.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'InvalidRequestException')
+            // You provided a parameter value that is not valid for the current state of the resource.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+        else if (err.code === 'ResourceNotFoundException')
+            // We can't find the resource that you asked for.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw err;
+    }
+    else {
+        // Decrypts secret using the associated KMS CMK.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if ('SecretString' in data) {
+            secret = data.SecretString;
+        } else {
+            let buff = new Buffer(data.SecretBinary, 'base64');
+            decodedBinarySecret = buff.toString('ascii');
+        }
+    }
+    console.log(JSON.stringify(data));
+    // Your code goes here.
+});
 
 const jsonProcess = (tree, fn) => {
   for (var attr in tree) {
